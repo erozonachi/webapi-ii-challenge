33c0b28a83b728b72dca75bd18a0abc78df7a03d
erozonachi <erozonachi.james@gmail.com>
2019-07-09 12:48:37 +0100

    Create, export, import and use posts routes in index

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const express = require('express');
+const postsRoutes = require('./routes/posts');
 
 const server = express();
 const port = process.env.PORT || 5000;
 
 server.use(express.json());
+server.use('/api/posts', postsRoutes);
 
 server.get('/', (req, res) => {
   res.status(200).send('Eneh\'s Posts API Service');
